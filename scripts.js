10e64db73a6d815e4191e1615a3ade8f18ea1987
susanavenda <susanavenda89@gmail.com>
2024-01-05 19:04:20 +0100

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -32,3 +32,85 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+
+
+ $(document).ready(function() {
+  $.getJSON('labels.json', function(data) {
+    try {
+      Object.keys(data).forEach(function(key) {
+        var element = $('#' + key);
+        if (element.length > 0) {
+          element.text(data[key]);
+        }
+      });
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }).fail(function(jqxhr, textStatus, error) {
+    console.error('Request Failed:', textStatus, error);
+  });
+});
+
+
+function mapToItem(data) {
+    if (data.jobTitle !== undefined) {
+        return {
+            title: data.jobTitle,
+            subtitle: data.company,
+            duration: data.duration,
+            details: Array.isArray(data.responsibilities) ? data.responsibilities : [data.responsibilities]
+        };
+    } else if (data.degree !== undefined) {
+        return {
+            title: data.degree,
+            subtitle: data.university,
+            duration: data.duration,
+            details: [] // Education might not have specific details in this context, you can add them if needed
+        };
+    } else {
+        return {}; // Return an empty object for unknown data
+    }
+}
+
+function createGenericElement(item) {
+    const itemElement = document.createElement("div");
+    itemElement.classList.add("d-flex", "flex-column", "flex-md-row", "justify-content-between", "mb-5");
+    itemElement.innerHTML = `
+        <div class="flex-grow-1">
+            <h3 class="mb-0">${item.title}</h3>
+            <div class="subheading mb-3">${item.subtitle}</div>
+            <ul>
+                ${item.details.map(detail => `<li>${detail}</li>`).join("")}
+            </ul>
+        </div>
+        <div class="flex-shrink-0"><span class="text-primary">${item.duration}</span></div>
+    `;
+    return itemElement;
+}
+
+function createObjectFromFile(file, elementId) {
+    // Get the container to append job elements
+    const jobsContainer = document.getElementById(elementId);
+
+    // Fetch the JSON file
+    fetch(file)
+        .then(response => response.json())
+        .then(items => {
+            // Create item elements and append to the container
+            items.forEach(item => {
+                const itemElement = createGenericElement(mapToItem(item));
+                jobsContainer.appendChild(itemElement);
+            });
+        })
+        .catch(error => {
+            console.error('Error fetching data:', error);
+        });
+}
+
+   createObjectFromFile('jobs.json', 'jobsList') ;
+ createObjectFromFile('education.json', 'educationList') ;
+  
+
+ 
+            
+
